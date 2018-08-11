062a314f0fe42ac914190c3f7fb0100ebd51b483
Tushar Sharma <tushar-sharma1729@gmail.com>
2018-08-11 23:32:21 +0000

    inverted index added

---
+++ MapReduce/InvertedIndex/InvertedIndex.java
@@ -0,0 +1,68 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+public class InvertedIndex {
+
+
+
+    public static class MapClass
+       extends Mapper<Object, Text, Text, Text>{
+
+        public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+
+            FileSplit fileSplit = (FileSplit) context.getInputSplit();
+            String filename = fileSplit.getPath().getName();
+
+
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                word.set(itr.nextToken());
+                context.write(word, new Text(filename));
+            }
+        }
+      }
+
+
+   public static class ReducerClass
+       extends Reducer<Text, Text,Text,Text> {
+    private Text result = new Text("1");
+
+    public void reduce(Text key, Iterable<Text> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      context.write(key, result);
+    }
+  }
+
+    public static void main(String[] args) throws Exception {
+
+
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(InvertedIndex.class);
+
+
+        job.setMapperClass(MapClass.class);
+        job.setReducerClass(ReducerClass.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+    }
+}
