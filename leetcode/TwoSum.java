66a1cff7d97337b4a1de5a07fa0f04ba85f7c18a
Tushar Sharma <tsharma@syncsort.com>
2018-11-13 02:48:56 +0000

    twosum added

--- leetcode/TwoSum.java
+++ leetcode/TwoSum.java
@@ -1,58 +1,43 @@
 /**
  *Problem: https://leetcode.com/problems/two-sum/description/
  */
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
 
     public static int[] twoSum(int[] nums, int target) {
 
+
+
         int[] result = new int[2];
+        HashMap<Integer, Integer> values = new HashMap<>();
 
 
-        //is nums sorted? , let me assume
         for (int i = 0; i < nums.length; i++){
-            int ret = search_binary(nums, target - nums[i], 0, nums.length - 1) ;
 
-            if(ret != - 1) {
-
-                result[0] = i;
-                result[1] = ret;
+            if (values.containsKey(target - nums[i])) {
+                result[1] = i;
+                result[0] = values.get(target - nums[i]);
             }
-        }
 
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i] + " ");
+            values.put(nums[i], i);
         }
 
         return result;
 
     }
 
-    //implement binary search
-    public static int search_binary(int[] nums, int target, int left, int right) {
-
-        //need a base case
-        if (left >= right) {
-            return -1;
-        }
-
-        int mid = left + (right - left) / 2;
-
-        if (nums[mid] == target) {
-            return mid;
-        }
-        else if (nums[mid] > target) {
-            return search_binary(nums, target, left, mid - 1);
-        }  else {
-            return search_binary(nums, target, mid + 1, right);
-        }
+    public static boolean isConsistent(int[] foo, int[] bar) {
+        return Arrays.equals(foo, bar);
     }
 
     public static void main(String[] args) {
 
-        int[] nums = {2,7,11,15};
+        //assert isConsistent(twoSum(new int[]{2, 7, 11, 15}, 9), new int[]{0, 1});
 
-        twoSum(nums, 9);
+        //twoSum(new int[]{3, 2, 3}, 6);
+        twoSum(new int[]{3, 2, 4}, 6);
 
     }
 
