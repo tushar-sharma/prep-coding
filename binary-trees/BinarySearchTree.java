1ae497a08d3ce99920d500f49c347d22ce2effba
tushar sharma <tushar.sharma1729@gmail.com>
2019-05-17 20:55:10 -0700

    Binary Search Tree added

--- binary-trees/BinarySearchTree.java
+++ binary-trees/BinarySearchTree.java
@@ -1,4 +1,9 @@
-
+/**
+ * Implement binary Search Tree
+ * following operations
+ * insert element
+ * search
+ */
 class Node<T> {
     T data;
     Node<T> left;
@@ -20,44 +25,72 @@ class Node<T> {
         return left;
     }
 
-    public Node<T> getRigth() {
+    public Node<T> getRight() {
         return right;
+    }
+
+    public void setLeft(Node<T> left) {
+        this.left = left;
+    }
+
+    public void setRight(Node<T> right) {
+        this.right = right;
     }
 }
 
 public class BinarySearchTree<T extends Comparable<T>> {
-    Node<T> root;
+
+    private Node<T> root;
 
     public BinarySearchTree() {
         root = null;
     }
 
     //insert data into a new node
-    public boolean insert(T data) {
-        Node<T> curr = root;
-        int comp = data.compareTo(curr.getData());
+    public void insert(T data) {
+        root = insert(data, root);
+    }
 
-        while (comp < 0 && curr.getLeft() != null) {
-            if (comp < 0) {
-                curr = curr.getLeft();
-            } else if (comp > 0) {
-                curr = curr.getRight();
-            } else {
-                return false;
-            }
+    public Node<T> insert(T data, Node<T> curr) {
+        if (curr == null) {
+            return new Node(data, curr);
         }
 
-        curr = new Node<T>(data, curr);
-        return true;
+        int comp = data.compareTo(curr.getData());
+
+        if (comp < 0) {
+            curr.setLeft(insert(data, curr.getLeft()));
+        } else if (comp > 0) {
+            curr.setRight(insert(data, curr.getRight()));
+        }
+
+        return curr;
+
+    }
+
+    public boolean preorder(Node<T> curr) {
+
+        if (curr == null) {
+            return false;
+        }
+
+        System.out.print(curr.getData() + " " );
+        preorder(curr.getLeft());
+        preorder(curr.getRight());
+
+        return  true;
     }
 
     public static void main(String[] args) {
-        String[] arrFill = {"1", "5", "2", "7", "4"};
+        String[] arrFill = {"50", "15", "72", "49", "73"};
 
         BinarySearchTree<String> bst = new BinarySearchTree<>();
 
         for (String st : arrFill) {
             bst.insert(st);
         }
+
+        bst.preorder(bst.root);
+        System.out.println();
     }
 }
