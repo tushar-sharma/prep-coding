24290b51220cda20cab4a970a1b26eeb0225c3df
Tushar Sharma <tushar.sharma1729@gmail.com>
2019-08-17 11:38:41 -0700

    stack added in js

---
+++ javascript/ex8.js
@@ -0,0 +1,12 @@
+/*
+ * create a stack and a queue
+ */
+const stack = [ ];
+stack.push(2);
+stack.push(3);
+console.log(stack.pop()); //print 3
+
+const queue = []
+queue.push(2);
+queue.push(3);
+console.log(queue.shift());
