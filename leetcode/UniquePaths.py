a84fd49789973fd588d091da492b701d6af1b17a
Tushar Sharma <tushar.sharma1729@gmail.com>
2021-09-14 05:29:23 +0100

    updated docs

---
+++ leetcode/UniquePaths.py
@@ -0,0 +1,19 @@
+"""
+Remember:
+1. How to declare 2d array in python
+"""
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        if m == n == 1:
+            return 1;
+        
+        dp = [[1] * n for i in range(m)]
+        
+        for row in range(1, len(dp)):
+            for col in range(1, len(dp[row])):
+                
+                dp[row][col] = dp[row][col - 1] + dp[row - 1][col]
+                
+                
+        return dp[m - 1][n - 1]
