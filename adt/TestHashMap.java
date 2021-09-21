21e1971b27b2a20980329051631d32ed212616df
Tushar Sharma <>
2021-09-21 01:13:04 -0400

    updated hashmap adt

---
+++ adt/TestHashMap.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+/*
+ * Remember
+ * containsKey is O(1)
+ * containsValue is O(n)
+ * Iterating over hashmap
+ */
+public class TestHashMap {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("a", 100);
+        map.put("b", 200);
+        map.put("c", 300);
+
+        //get key
+        //O(1) complexity
+        System.out.println("key for b is " + map.getOrDefault("b", 0));
+
+        System.out.println("key for z is " + map.getOrDefault("z", 0));
+
+        if (map.containsKey("a")) {
+            System.out.println("a exist as key");
+        }
+
+        if (map.containsValue(100)) {
+            System.out.println("100 exist as value");
+        }
+
+        map.forEach ( (key, value) -> {
+            System.out.println("key is " + key + " value is " + value);
+        });
+
+    }
+}
