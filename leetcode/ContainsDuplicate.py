bcedde4789bf896cdb1e0b524b7ae138308b71d4
Tushar Sharma <>
2021-09-21 01:40:34 -0400

    updated

--- leetcode/ContainsDuplicate.py
+++ leetcode/ContainsDuplicate.py
@@ -1,16 +1,18 @@
+from collections import defaultdict
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+
 
         if len(nums) == 1:
             return False
 
-        track = {}
+        track = defaultdict(list)
 
         for num in nums:
-            if num in track:
+            if track.get(num):
                 return True
             else:
                 track[num] = 1
 
         return False
-
