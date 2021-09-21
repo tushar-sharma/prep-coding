025e0761d32c95e7569849ca14550dc54c8dd243
Tushar Sharma <tushar-sharma@users.noreply.github.com>
2021-09-21 01:22:52 -0400

    Update ContainsDuplicate.java

--- leetcode/ContainsDuplicate.java
+++ leetcode/ContainsDuplicate.java
@@ -5,9 +5,7 @@ class Solution {
             return false;
         }
 
-
         Map<Integer, Integer> map = new HashMap<>();
-
 
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
