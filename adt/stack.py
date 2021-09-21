ffb6271f95fe5bf92d2785a9ba5d8c9ec73da673
Tushar Sharma <>
2021-09-20 22:52:12 -0400

    stack adt added python

---
+++ adt/stack.py
@@ -0,0 +1,33 @@
+from collections import deque
+"""
+Using stack in python
+"""
+
+st = deque()
+
+print("Pushing element 20")
+st.append(20)
+
+print("Pushing element -2")
+st.append(-2)
+
+print("Pushing element 5")
+st.append(5)
+
+print("Top element in stack is %s" % st[-1])
+
+assert st[-1] == 5
+
+print("Pop latest element %s" % st.pop())
+
+print("Pop latest element %s" % st.pop())
+
+print("Top element in stack is %s" % st[-1])
+
+assert st[-1] == 20
+
+
+print("Pusing element in stack 100")
+st.append(100)
+
+print("print the stack %s" % st)
