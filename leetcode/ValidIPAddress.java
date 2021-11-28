c1291141f96e3d9d8ac953481b670ea6f04deede
Tushar Sharma <>
2021-11-28 15:20:16 -0500

    updated leetcode

---
+++ leetcode/ValidIPAddress.java
@@ -0,0 +1,88 @@
+/**
+Things to remember:
+1. Spilt a string by dot requires double slash
+2. Get actual value of a number from character
+   int val = (int) c - 48;
+*/
+class Solution {
+
+    public boolean checkIPv4(String queryIP) {
+
+        String[] queryIPArray = queryIP.split("\\.", -1);
+
+        if (queryIPArray.length != 4) {
+            return false;
+        }
+
+        for(String query : queryIPArray) {
+
+            if (query.isEmpty()){
+                return false;
+            }
+
+            if (query.length() > 1 && query.charAt(0) == '0'){
+                return false;
+            }
+
+            int val = 0;
+            int factor = 1;
+            for (char c : query.toCharArray()) {
+
+                if (!Character.isDigit(c)) {
+                    return false;
+                }
+
+                val = (val * factor)  + (int) c - 48;
+
+                factor = 10;
+            }
+
+            if (val < 0 || val > 255) {
+                return false;
+            }
+
+        }
+
+        return true;
+
+
+    }
+
+    public boolean checkIPv6(String queryIP) {
+        String[] queryIPArray = queryIP.split(":", -1);
+
+
+        if (queryIPArray.length != 8) {
+            return false;
+        }
+
+        for(String query : queryIPArray) {
+
+            if (query.length() < 1 || query.length() > 4){
+                return false;
+            }
+
+            for (char c : query.toCharArray()) {
+                if (! ((c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F') || (c >= '0' && c <= '9')) ){
+                    return false;
+                }
+            }
+
+
+        }
+
+        return true;
+    }
+
+    public String validIPAddress(String queryIP) {
+
+        if (checkIPv4(queryIP)){
+            return "IPv4";
+        } else if (checkIPv6(queryIP)){
+            return "IPv6";
+        } else {
+            return "Neither";
+        }
+
+    }
+}
