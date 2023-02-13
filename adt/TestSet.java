78944f20a330c8538f0acfbf212e480b11694c24
Tushar Sharma <tushar-sharma@users.noreply.github.com>
2023-02-13 01:29:27 -0500

    Update TestSet.java

--- adt/TestSet.java
+++ adt/TestSet.java
@@ -6,10 +6,14 @@ public class Test{
         mySet.add(1);
         mySet.add(2);
         mySet.add(30);
-    
+   
         if(myset.contains(30)){
             System.out.println("contains 30");
         }
+    
+        //tricks
+        String[] foo = {"foo1", "foo2", "foo3"};
+        Set<String> newSet = new HashSet<>(Arrays.asList(foo));
     
   }
 }
