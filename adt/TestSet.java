43bc96f0ca2d31d5a521fe2287ed0beef290c3f5
Tushar Sharma <tushar-sharma@users.noreply.github.com>
2023-02-13 01:27:34 -0500

    Create TestSet.java

---
+++ adt/TestSet.java
@@ -0,0 +1,15 @@
+public class Test{
+  public static void main(String[] args) {
+        Set<Integer> mySet = new HashSet<>();
+    
+        //add element
+        mySet.add(1);
+        mySet.add(2);
+        mySet.add(30);
+    
+        if(myset.contains(30)){
+            System.out.println("contains 30");
+        }
+    
+  }
+}
