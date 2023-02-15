0397273ff17a2644d7c86cb4a8a6618201c622dd
Tushar Sharma <tushar-sharma@users.noreply.github.com>
2023-02-14 22:13:30 -0500

    Update TestSet.java

--- adt/TestSet.java
+++ adt/TestSet.java
@@ -1,18 +1,22 @@
 public class Test{
   public static void main(String[] args) {
+        //O(1)
         Set<Integer> mySet = new HashSet<>();
     
-        //add element
+        //O(1)
         mySet.add(1);
         mySet.add(2);
         mySet.add(30);
    
+        //O(1)
+        //it only requires calculating the hash code of the element and checking if it exists in the hash table.
         if(myset.contains(30)){
             System.out.println("contains 30");
         }
     
         //convert array to hashset
         String[] foo = {"foo1", "foo2", "foo3"};
+        //O(n)
         Set<String> newSet = new HashSet<>(Arrays.asList(foo));
     
   }
